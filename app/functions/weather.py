b58dfa0016e2ecdd1549646c520e0685b7b241b5
Angel Racini <angel.racini@rappi.com>
2022-06-01 20:47:23 -0500

    add-description-function-calculate-weather

--- app/functions/weather.py
+++ app/functions/weather.py
@@ -2,4 +2,10 @@ from app.models.weather import Weather
 
 
 def calculateWeather(planets : list) -> Weather:
+    """
+    Calculates the weather based on the position of the planets given in the list planets
+
+    :param planets: List[Planet]
+    :return: Weather
+    """
     return Weather(1,"lluvia")
