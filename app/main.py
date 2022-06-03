62879f904ab9cf25bc90d2e8723aaa0b27dff805
Angel Racini <angel.racini@rappi.com>
2022-06-02 23:24:09 -0500

    modify-swagger-docs

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ from fastapi import FastAPI
 from app.routes.weather import weather
 
 API_description = """ 
-**Vulcano-Weather API helps Vulcano people to know the weather of the planet in the next 10 years**
+**Vulcano-Weather API helps Vulcano people to know the weather 
+of the planet in the next 10 years before it was destroyed by Nero**
+
+In this swagger doc you can see the API Documentation and the different routes
+
+Enjoy it..
 """
 
 app = FastAPI(title="Vulcano Weather",
@@ -14,12 +19,21 @@ app = FastAPI(title="Vulcano Weather",
                   "name": "Angel Racini",
                   "url": "https://github.com/arracinim/vulcano-weather",
                   "email": "angelricardoracinimeza@gmail.com"
-              }, )
+              })
 
 app.include_router(weather)
 
 
-@app.get("/")
+@app.get("/", name= "Health Check",
+         summary= "Check if the API is UP",
+         tags=["Health Check"],
+         responses={
+             200: {
+                 "content": {
+                     "application/json": {
+                         "example": {"health_check": "up"}
+                     }}}}
+         )
 def health_check():
     return {"health_check": "up"}
 
